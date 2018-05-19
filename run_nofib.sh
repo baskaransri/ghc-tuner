b491d2ff9cfa0c0c062addf3a0eca17ac1ca3ff0
Kavon Farvardin <kavon@farvard.in>
2018-05-19 16:27:44 -0500

    nofib runner script

---
+++ run_nofib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# inputs:
+EXTRA_FLAG=$1
+OUTPUT_FILE=$2
+TRIALS=$3
+
+set -e
+
+pushd ghc/nofib
+make clean
+make boot
+make -k NoFibRuns=$TRIALS EXTRA_HC_OPTS=$EXTRA_FLAG 2>&1 | tee ./nofib-log
+popd
+mv ghc/nofib/nofib-log $OUTPUT_FILE
